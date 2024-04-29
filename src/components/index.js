329c570bb7057efb8353492b6aa75aa81353ab21
Monique Mendes <moniiquemendes96@gmail.com>
2024-04-29 11:44:14 +0200

    formulario de login + css

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export * from "./FeedLogic/FeedLogicContent";
 export * from "./Comment/CreateComment";
 export * from "./Comment/CreateComment.css";
 export * from "./FeedLogic/FeedLogicContent.css";
+export * from "./Login/LoginForm";
+export * from "./Login/LoginForm.css";
